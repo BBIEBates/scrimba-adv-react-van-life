a8e2bec48a0e132fa7f693875517a0bc6d37550b
BBIEBates <ebates@bbilogistics.com>
2023-12-21 16:21:27 -0500

    added nested routes with relative path + host layout with React Outlet component

---
+++ pages/Host/Income.jsx
@@ -0,0 +1,8 @@
+import React from "react"
+
+export default function Income() {
+
+    return (
+        <h1>This is the Income component</h1>
+    )
+}
---
+++ pages/Host/Reviews.jsx
@@ -0,0 +1,8 @@
+import React from "react"
+
+export default function Reviews() {
+
+    return (
+        <h1>This is the Reviews component</h1>
+    )
+}
