979be20fb2a1e87571eb20edad408003a18a6aee
EMILY_BATES <ebates@bbilogistics.com>
2024-03-22 13:49:39 -0400

    removing repo from local

--- pages/Host/HostVanDetail.jsx
+++ pages/Host/HostVanDetail.jsx
@@ -1,7 +1,17 @@
 import React from "react"
+import { useParams } from "react-router-dom"
 
 export default function HostVanDetail() {
+    const [currentVan, setCurrentVan] = React.useState(null);
+    const {id} = useParams();
+
+    React.useEffect(() => {
+        fetch(`/api/host/vans/${id}`)
+            .then(res => res.json())
+            .then(data => setCurrentVan(data.vans[0]))
+    }, []);
+console.log(currentVan)
     return (
-        <h1>Host Van Detail Page</h1>
+        <h1>Host Van Detail Page for van {id}</h1>
     )
 }
